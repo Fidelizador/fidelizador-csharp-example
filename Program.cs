1280fba802868503b7c72523c6a469d5e7e346b4
Nicolas Aravena <naravena@fidelizador.com>
2022-02-09 15:27:41 -0300

    Encapsulate all api execution to get error details

--- Program.cs
+++ Program.cs
@@ -14,39 +14,51 @@ namespace Result
 
             var api = new FidelizadorClient(client_id, client_secret, client_slug);
             Console.WriteLine("/=======================================/");
-            Console.WriteLine("Obtaining all lists in the instance");
-            var lists = api.GetLists();
-            lists.Wait();
-            Console.Write("Printing first result: ");
-            Console.WriteLine(lists.Result!.Data[0]);
-            Console.WriteLine("/=======================================/");
-            Console.WriteLine("Creating a new list called 'new list'");
-            var newList = api.CreateList("new list");
-            newList.Wait();
-            Console.Write("New list id: ");
-            Console.WriteLine(newList.Result!.Id);
-            Console.WriteLine("/=======================================/");
-            Console.WriteLine("Starting a new import on the new list");
-            string filePath = Path.GetFullPath("./files/example.csv");
-            Console.WriteLine(filePath);
-            var importResult = api.StartImport(newList.Result.Id, filePath);
-            importResult.Wait();
-            Console.Write("New import result: ");
-            Console.WriteLine(importResult.Result);
-            Console.WriteLine("/=======================================/");
-            Console.WriteLine("Creating a new campaign");
-            var campaign = api.CreateCampaign(
-                "My campaign from c#",
-                1,
-                newList.Result.Id,
-                1,
-                "Welcome to my Campaign",
-                "My company",
-                "contact@example.org",
-                "contact@example.org");
-            campaign.Wait();
-            Console.Write("New Campaign: ");
-            Console.WriteLine(campaign.Result);
+            try {
+                Console.WriteLine("Obtaining all lists in the instance");
+                var lists = api.GetLists();
+                lists.Wait();
+                Console.Write("Printing first result: ");
+                Console.WriteLine(lists.Result!.Data[0]);
+                Console.WriteLine("/---------------------------------------/");
+                Console.WriteLine("Creating a new list called 'new list'");
+                var newList = api.CreateList("new list");
+                newList.Wait();
+                Console.Write("New list id: ");
+                Console.WriteLine(newList.Result!.Id);
+                Console.WriteLine("/---------------------------------------/");
+                Console.WriteLine("Starting a new import on the new list");
+                string filePath = Path.GetFullPath("./files/example.csv");
+                Console.WriteLine(filePath);
+                var importResult = api.StartImport(newList.Result.Id, filePath);
+                importResult.Wait();
+                Console.Write("New import result: ");
+                Console.WriteLine(importResult.Result);
+                Console.WriteLine("/---------------------------------------/");
+                Console.WriteLine("Creating a new campaign");
+                var campaign = api.CreateCampaign(
+                    "My campaign from c#",
+                    1,
+                    newList.Result.Id,
+                    1,
+                    "Welcome to my Campaign",
+                    "My company",
+                    "contact@example.org",
+                    "contact@example.org");
+                campaign.Wait();
+                Console.Write("New Campaign: ");
+                Console.WriteLine(campaign.Result);
+
+            } catch (AggregateException err) {
+                Console.WriteLine("/---------------------------------------/");
+                foreach (var errInner in err.InnerExceptions) {
+                    if (errInner is HttpRequestException) {
+                        Console.WriteLine("Error retrieving information. Please check your Fidelizador slug and credentials.");
+                    } else {
+                        Console.WriteLine(errInner);
+                    }
+                }
+            }
             Console.WriteLine("/=======================================/");
         }
     }
